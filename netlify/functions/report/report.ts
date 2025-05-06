395aa06b512ef74aa0e905a619dc89ce5d281d84
Rien Heuver <ik@rienheuver.nl>
2025-05-06 11:38:13 +0200

    feat: process multiple pageblocks

--- netlify/functions/report/report.ts
+++ netlify/functions/report/report.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
-import puppeteer from "puppeteer-core";
+import { PDFDocument } from "pdf-lib";
+import puppeteer, { ElementHandle } from "puppeteer-core";
 
 type PageBlock = {
   cover: boolean;
@@ -29,13 +30,13 @@ export const handler = async (req) => {
   console.log("Starting pdf generation...");
 
   const {
-    block,
+    pageBlocks,
     styles,
     logoDataUrl,
     title,
     headerColor,
   }: {
-    block: PageBlock;
+    pageBlocks: PageBlock[];
     styles: string[];
     logoDataUrl: string;
     title: string;
@@ -49,123 +50,139 @@ export const handler = async (req) => {
 
   page.on("error", (error) => console.error(error));
 
-  await page.setContent(block.content);
+  const pdfBlocks: Uint8Array[] = [];
+  for (const [index, block] of pageBlocks.entries()) {
+    console.log(`Creating pdf ${index + 1} of ${pageBlocks.length}`);
+    console.log(block.content);
 
-  const styleTagPromises = [
-    page.addStyleTag({
-      url: "https://unpkg.com/open-props@1.7.15/open-props.min.css",
-    }),
-    page.addStyleTag({
-      url: "https://unpkg.com/open-props@1.7.15/normalize.light.min.css",
-    }),
-    page.addStyleTag({
-      url: "https://fonts.bunny.net/css?family=poppins:100,400,900",
-    }),
-    page.addStyleTag({
-      url: "https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css",
-    }),
-    page.addStyleTag({
-      content: `
-        html {
-          -webkit-print-color-adjust: exact !important;
-          font-size: 14px;
-          background: white;
+    await page.setContent(block.content, {
+      waitUntil: ["domcontentloaded", "load", "networkidle0"],
+    });
+
+    const styleTagPromises: Promise<ElementHandle>[] = [
+      page.addStyleTag({
+        url: "https://unpkg.com/open-props@1.7.15/open-props.min.css",
+      }),
+      page.addStyleTag({
+        url: "https://unpkg.com/open-props@1.7.15/normalize.light.min.css",
+      }),
+      page.addStyleTag({
+        url: "https://fonts.bunny.net/css?family=poppins:100,400,900",
+      }),
+      page.addStyleTag({
+        url: "https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css",
+      }),
+      page.addStyleTag({
+        content: `
+          html {
+            -webkit-print-color-adjust: exact !important;
+            font-size: 16px;
+            background: white;
+            }
+            `,
+      }),
+      page.addStyleTag({
+        content: `
+          @page {
+            size: a4 landscape;
+            padding-block: ${block.cover ? "0" : "1.05in"};
+            padding-inline: ${block.cover ? "0" : ".5in"};
           }
-          `,
-    }),
-    page.addStyleTag({
-      content: `
-        @page {
-          size: a4 landscape;
-          padding-block: ${block.cover ? "0" : "1.05in"};
-          padding-inline: ${block.cover ? "0" : ".5in"};
-        }
-      `,
-    }),
-  ];
-  for (const style of styles) {
-    styleTagPromises.push(page.addStyleTag({ content: style }));
+        `,
+      }),
+    ];
+    for (const style of styles) {
+      styleTagPromises.push(page.addStyleTag({ content: style }));
+    }
+    await Promise.all(styleTagPromises);
+    page.waitForNetworkIdle();
+    // For colour usage
+    await page.setBypassCSP(true);
+    await page.emulateMediaType("print");
+    pdfBlocks.push(
+      await page.pdf({
+        format: "A4",
+        preferCSSPageSize: true,
+        displayHeaderFooter: !block.cover,
+        printBackground: true,
+        headerTemplate: `
+        <style>
+          html {
+            -webkit-print-color-adjust: exact !important;
+          }
+          .header {
+            display: flex;
+            flex-direction: column;
+            gap: 32px;
+            color: #495057;
+            font-size: 16px;
+            width: 100%;
+            padding-inline: .5in;
+
+            .info {
+              display: flex;
+              margin-inline: 64px;
+              justify-content: space-between;
+
+              /* Cannot use page numbering because of split-up page generation
+              Check this perhaps: https://github.com/puppeteer/puppeteer/issues/3383
+              */
+              .pageNumber {
+                display: none;
+              }
+            }
+
+            hr {
+              margin: 0;
+              padding: 0;
+              border: 5px solid ${headerColor};
+              width: 100%;
+            }
+          }
+        </style>
+        <div class="header">
+          <hr />
+          <div class="info">
+            <span>${title}</span>
+            <span class="pageNumber"></span>
+          </div>
+        </div>
+        `,
+        footerTemplate: `
+        <style>
+          footer {
+            display: flex;
+            justify-content: flex-end;
+            width: 100%;
+            height: 80px;
+            padding-inline: .5in;
+          }
+
+          .logo {
+            content: url("${logoDataUrl}");
+            width: 150px;
+            object-fit: contain;
+          }
+        </style>
+        <footer>
+          <span class="logo"></span>
+        </footer>
+        `,
+      })
+    );
   }
-  await Promise.all(styleTagPromises);
-  // For colour usage
-  await Promise.all([
-    page.waitForNetworkIdle(),
-    // page.setBypassCSP(true),
-    // page.emulateMediaType('print'),
-  ]);
-  const pdfBytes = await page.pdf({
-    // format: 'A4',
-    preferCSSPageSize: true,
-    displayHeaderFooter: !block.cover,
-    printBackground: true,
-    headerTemplate: `
-    <style>
-      html {
-        -webkit-print-color-adjust: exact !important;
-      }
-      .header {
-        display: flex;
-        flex-direction: column;
-        gap: 32px;
-        color: #495057;
-        font-size: 14px;
-        width: 100%;
-        padding-inline: .5in;
-
-        .info {
-          display: flex;
-          margin-inline: 64px;
-          justify-content: space-between;
-
-          /* Cannot use page numbering because of split-up page generation
-          Check this perhaps: https://github.com/puppeteer/puppeteer/issues/3383
-          */
-          .pageNumber {
-            display: none;
-          }
-        }
-
-        hr {
-          margin: 0;
-          padding: 0;
-          border: 5px solid ${headerColor};
-          width: 100%;
-        }
-      }
-    </style>
-    <div class="header">
-      <hr />
-      <div class="info">
-        <span>${title}</span>
-        <span class="pageNumber"></span>
-      </div>
-    </div>
-    `,
-    footerTemplate: `
-    <style>
-      footer {
-        display: flex;
-        justify-content: flex-end;
-        width: 100%;
-        height: 80px;
-        padding-inline: .5in;
-      }
-
-      .logo {
-        content: url("${logoDataUrl}");
-        width: 150px;
-        object-fit: contain;
-      }
-    </style>
-    <footer>
-      <span class="logo"></span>
-    </footer>
-    `,
-  });
 
   await browser.close();
+  console.log("Done, now merging pdf's");
 
-  console.log("PDF completed");
+  const finalPdf = await PDFDocument.create();
+  for (const pdf of pdfBlocks) {
+    const doc = await PDFDocument.load(pdf);
+    const pages = await finalPdf.copyPages(doc, doc.getPageIndices());
+    pages.forEach((p) => finalPdf.addPage(p));
+  }
+  const pdfBytes = await finalPdf.save();
+  console.log("Final pdf completed");
 
   return {
     statusCode: 200,
